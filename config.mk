d7a8486476acdc819e2a2e1ca0c306baa810e912
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2021-02-25 08:58:44 +0000

    config.mk: /usr/bin -> /usr/sbin

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 NAME      = pkgutils
 VERSION   = 5.40.7
 
-BINDIR    = /usr/bin
+BINDIR    = /usr/sbin
 MANDIR    = /usr/share/man
 ETCDIR    = /etc
 
@@ -14,5 +14,6 @@ CPPFLAGS  = -D_POSIX_SOURCE -D_GNU_SOURCE -DVERSION=\"$(VERSION)\" \
             -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
 
 LDFLAGS  += -static $(shell pkg-config --libs --static libarchive)
+#LDFLAGS  += -larchive
 
 # End of file.
