e5ddb6c08905b3ce7b993aa22c49ef294f2f0371
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-01-14 06:13:01 +0200

    config.mk: rm -ldl from LDFLAGS

--- config.mk
+++ config.mk
@@ -10,4 +10,4 @@ MANDIR  = $(PREFIX)/share/man
 CXXFLAGS += -Wall -Wextra -pedantic
 CPPFLAGS += -D_POSIX_SOURCE -D_GNU_SOURCE -DVERSION=\"$(VERSION)\" \
             -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG
-LDFLAGS += -static $(shell pkg-config --libs --static libarchive) -ldl
+LDFLAGS += -static $(shell pkg-config --libs --static libarchive)
