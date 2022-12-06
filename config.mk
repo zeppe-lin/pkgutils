a2efcdd920f734bf9a33463d9da2667094dd0cc2
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-06 10:14:01 +0200

    config.mk: add CXX and LD variables

--- config.mk
+++ config.mk
@@ -14,5 +14,9 @@ CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -DVERSION=\"${VERSION}\" \
            -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG
 LDFLAGS  = -static $(shell pkg-config --libs --static libarchive)
 
+# compiler
+CXX = c++
+LD  = ${CXX}
+
 # vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
 # End of file.
