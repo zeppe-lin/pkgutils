82f040163badd85f685264aed44c81f6f8dba509
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-06 10:06:15 +0200

    config.mk: don't append flags

--- config.mk
+++ config.mk
@@ -1,4 +1,3 @@
-# This file is a part of pkgutils.
 # See COPYING and COPYRIGHT files for corresponding information.
 
 # pkgutils version if undefined
@@ -10,10 +9,10 @@ BINDIR  = ${PREFIX}/sbin
 MANDIR  = ${PREFIX}/share/man
 
 # flags
-CXXFLAGS += -Wall -Wextra -pedantic
-CPPFLAGS += -D_POSIX_SOURCE -D_GNU_SOURCE -DVERSION=\"${VERSION}\" \
-            -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG
-LDFLAGS += -static $(shell pkg-config --libs --static libarchive)
+CXXFLAGS = -Wall -Wextra -pedantic
+CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -DVERSION=\"${VERSION}\" \
+           -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DNDEBUG
+LDFLAGS  = -static $(shell pkg-config --libs --static libarchive)
 
 # vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
 # End of file.
