0bff07721c19255567f3c4f9d2bc2fe4af49bf91
Alexandr Savca <alexandr.savca89@gmail.com>
2022-12-21 18:36:27 +0200

    main.cc: remove unnecessary option handler, add whitespace to includes

--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include <memory>
 #include <cstdlib>
 #include <libgen.h>
+
 #include "pkgutil.h"
 #include "pkgadd.h"
 #include "pkgrm.h"
@@ -35,24 +36,6 @@ int main(int argc, char** argv)
   try
   {
     unique_ptr<pkgutil> util(select_utility(name));
-
-    // Handle common options
-    for (int i = 1; i < argc; i++)
-    {
-      string option(argv[i]);
-
-      if (option == "-v" || option == "--version")
-      {
-        util->print_version();
-        return EXIT_SUCCESS;
-      }
-      else if (option == "-h" || option == "--help")
-      {
-        util->print_help();
-        return EXIT_SUCCESS;
-      }
-    }
-
     util->run(argc, argv);
   }
   catch (runtime_error& e)
