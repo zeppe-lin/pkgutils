ccc1e7b420b51716c030e40651dc5af82e3bd38c
Alexandr Savca <alexandr.savca89@gmail.com>
2023-01-03 01:05:42 +0200

    config.mk: reorder flags

--- config.mk
+++ config.mk
@@ -2,9 +2,9 @@
 VERSION = 5.41
 
 # flags
-CXXFLAGS = -Wall -Wextra -pedantic
 CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	   -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
+CXXFLAGS = -Wall -Wextra -pedantic
 LDFLAGS  = -static $(shell pkg-config --libs --static libarchive)
 
 # compiler
