9c826a0d5f41839bcc5a73e5e7ab112f866a1643
Alexandr Savca <alexandr.savca89@gmail.com>
2023-01-07 14:30:01 +0200

    config.mk: add -Wformat to CXXFLAGS

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 5.41
 # flags
 CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	   -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
-CXXFLAGS = -Wall -Wextra -pedantic
+CXXFLAGS = -Wall -Wextra -pedantic -Wformat
 LDFLAGS  = -static $(shell pkg-config --libs --static libarchive)
 
 # compiler
