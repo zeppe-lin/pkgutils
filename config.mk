658dd271ba96a8f60ef030819e60bd12bb9cef6e
Alexandr Savca <alexandr.savca89@gmail.com>
2023-01-07 14:45:59 +0200

    config.mk: add -Wformat-security, -Wconversion, -Wsign-conversion to CXXFLAGS

--- config.mk
+++ config.mk
@@ -4,7 +4,8 @@ VERSION = 5.41
 # flags
 CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	   -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
-CXXFLAGS = -Wall -Wextra -pedantic -Wformat
+CXXFLAGS = -Wall -Wextra -pedantic -Wformat -Wformat-security \
+	   -Wconversion -Wsign-conversion
 LDFLAGS  = -static $(shell pkg-config --libs --static libarchive)
 
 # compiler
