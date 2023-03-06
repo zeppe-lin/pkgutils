d8e39edbcf17c18eed94975723dce9bc59b771c4
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-06 04:21:03 +0200

    config.mk: add paths

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
 # pkgutils version
 VERSION = 5.41
 
+# paths
+PREFIX = /usr
+MANPREFIX = ${PREFIX}/share/man
+
 # flags
 CPPFLAGS = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	   -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
