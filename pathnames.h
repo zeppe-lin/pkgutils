8f05251207e8ecd314ef412d220b9f4fa92b7875
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-04 10:11:59 +0300

    pathnames.h: indent fix

--- pathnames.h
+++ pathnames.h
@@ -11,25 +11,25 @@
 #define PKGADD_CONF_MAXLINE     1024
 
 //!< Default package extension.
-#define PKG_EXT         ".pkg.tar."
+#define PKG_EXT                 ".pkg.tar."
 
 //!< Default path for package database.
-#define PKG_DIR         "var/lib/pkg"
+#define PKG_DIR                 "var/lib/pkg"
 
 //!< Default package database location.
-#define PKG_DB          "var/lib/pkg/db"
+#define PKG_DB                  "var/lib/pkg/db"
 
 //!< Default path for rejected files.
-#define PKG_REJECTED    "var/lib/pkg/rejected"
+#define PKG_REJECTED            "var/lib/pkg/rejected"
 
 //!< Default package's name#version delimiter.
-#define VERSION_DELIM   '#'
+#define VERSION_DELIM           '#'
 
 //!< Default location for ldconfig(8) binary.
-#define LDCONFIG        "/sbin/ldconfig"
+#define LDCONFIG                "/sbin/ldconfig"
 
 //!< Default location for ldconfig(8) configuration file.
-#define LDCONFIG_CONF   "/etc/ld.so.conf"
+#define LDCONFIG_CONF           "/etc/ld.so.conf"
 
 // vim:sw=2:ts=2:sts=2:et:cc=72:tw=70
 // End of file.
