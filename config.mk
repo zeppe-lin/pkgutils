71406f0988adc920bb6b97102a58dd65de89a51a
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-18 01:11:09 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,21 +1,21 @@
 # project metadata
-NAME        = pkgutils
-VERSION     = 6.00
-DESCRIPTION = Package Management Utilities
-DIST        = ${NAME}-${VERSION}
+NAME         = pkgutils
+VERSION      = 6.00
+DESCRIPTION  = Package Management Utilities
+DIST         = ${NAME}-${VERSION}
 
 # paths
-PREFIX      = /usr
-MANPREFIX   = ${PREFIX}/share/man
-BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
+PREFIX       = /usr
+MANPREFIX    = ${PREFIX}/share/man
+BASHCOMPDIR  = ${PREFIX}/share/bash-completion/completions
 
 # flags
-CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
-	      -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
-CXXFLAGS    = -Wall -Wextra -pedantic -Wformat -Wformat-security \
-	      -Wconversion -Wsign-conversion
-LDFLAGS     = -static $(shell pkg-config --libs --static libarchive)
+CPPFLAGS     = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
+	       -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
+CXXFLAGS     = -Wall -Wextra -pedantic -Wformat -Wformat-security \
+	       -Wconversion -Wsign-conversion
+LDFLAGS      = -static $(shell pkg-config --libs --static libarchive)
 
 # compiler
-CXX = c++
-LD  = ${CXX}
+CXX          = c++
+LD           = ${CXX}
