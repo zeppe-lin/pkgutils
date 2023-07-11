64d10946bc79b78eddd5ea85de07b587f65a5828
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-11 08:38:16 +0300

    pkgutil.cc: add zstd support

--- pkgutil.cc
+++ pkgutil.cc
@@ -35,6 +35,7 @@
   archive_read_support_filter_bzip2((ar));  \
   archive_read_support_filter_xz((ar));     \
   archive_read_support_filter_lzip((ar));   \
+  archive_read_support_filter_zstd((ar));   \
   archive_read_support_format_tar((ar))
 
 #define DEFAULT_BYTES_PER_BLOCK (20 * 512)
