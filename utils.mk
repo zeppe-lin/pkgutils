b36c31771c8af47df55dbfb81d62d32ab002f106
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-02 06:10:26 +0300

    utils.mk: add all to .PHONY, more words for targets descriptions

--- utils.mk
+++ utils.mk
@@ -1,33 +1,33 @@
 all: deadlinks podchecker shellcheck cppcheck flawfinder longlines
 
 deadlinks:
-	@echo "=======> deadlinks"
-	@grep -EIihor "https?://[^\"\\'> ]+" --exclude-dir=.git*      \
-		| xargs -P10 -r -I{}                                  \
-		  curl -o/dev/null -sw "[%{http_code}] %{url}\n" '{}' \
-		| grep -v '^\[200\]'                                  \
+	@echo "=======> Check for dead links"
+	@grep -EIihor "https?://[^\"\\'> ]+" --exclude-dir=.git*  \
+		| xargs -P10 -r -I{} curl -o/dev/null             \
+		  -sw "[%{http_code}] %{url}\n" '{}'              \
+		| grep -v '^\[200\]'                              \
 		| sort -u
 
 podchecker:
-	@echo "=======> podchecker"
+	@echo "=======> Check PODs for syntax errors"
 	@podchecker *.pod
 
 shellcheck:
-	@echo "=======> shellcheck"
+	@echo "=======> Check shell scripts for syntax errors"
 	@grep -m1 -Irl '^#\s*!/bin/sh' --exclude-dir=.git* \
 		| xargs -L10 -r shellcheck -s sh
 
 cppcheck:
-	@echo "=======> cppcheck"
+	@echo "=======> Static C/C++ code analysis"
 	@cppcheck --quiet --enable=all --check-level=exhaustive \
 		--suppress=missingIncludeSystem .
 
 flawfinder:
-	@echo "=======> flawfinder"
+	@echo "=======> Check for potential security flaws"
 	@flawfinder -D --quiet .
 
 longlines:
-	@echo "=======> long (>80) lines"
-	@grep -PIrn '^.{81,}$$' --exclude-dir=.git* || :
+	@echo "=======> Check for long lines"
+	@! grep -PIrn '^.{81,}$$' --exclude-dir=.git*
 
-.PHONY: deadlinks podchecker shellcheck cppcheck longlines
+.PHONY: all deadlinks podchecker shellcheck cppcheck longlines
