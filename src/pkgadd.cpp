9cf00e44dd7f8bfe8ad92ab827d07032208c2e6b
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-17 20:20:58 +0300

    pkgadd.cpp: remove print_version(), don't hardcode project's homepage and bugtracker

--- src/pkgadd.cpp
+++ src/pkgadd.cpp
@@ -226,23 +226,9 @@ Mandatory arguments to long options are mandatory for short options too.
   -v, --verbose        explain what is being done
   -V, --version        print version and exit
   -h, --help           print help and exit
-
-Report bugs to: <https://github.com/zeppe-lin/pkgutils/issues/>
-pkgutils home page: <https://github.com/zeppe-lin/pkgutils/>
 )END";
-}
-
-void pkgadd::print_version() const
-{
-  cout << utilname << " (pkgutils) " << VERSION << R"END(
-Copyright (C) 2000-2005 Per Lidén   <per@fukt.bth.se>
-Copyright (C) 2006-2017 CRUX team   <http://crux.nu>
-Copyright (C) 2002-2021 Håvard Moen <vanilje@netcom.no>
-Copyright (C) 2021-2023 Zeppe-Lin   <http://zeppel.ink>
-License GPLv2+: GNU GPL version 2 or later <https://gnu.org/licenses/gpl.html>.
-This is free software: you are free to change and redistribute it.
-There is NO WARRANTY, to the extent permitted by law.
-)END";
+  cout << "Report bugs to: <"     << PROJECT_BUGTRACKER << ">" << endl;
+  cout << "pkgutils home page: <" << PROJECT_HOMEPAGE   << ">" << endl;
 }
 
 void pkgadd::run(int argc, char** argv)
