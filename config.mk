d3c0a8e537ebb02b7bfaa22e7561869e87f30f05
Alexandr Savca <alexandr.savca89@gmail.com>
2023-08-18 11:34:45 +0300

    config.mk: add CPPFLAGS with metadata

--- config.mk
+++ config.mk
@@ -13,7 +13,10 @@ BASHCOMPDIR  = ${PREFIX}/share/bash-completion/completions
 
 # flags
 CPPFLAGS     = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
-	       -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
+	       -D_FILE_OFFSET_BITS=64 -DNDEBUG \
+	       -DPROJECT_VERSION=\"${VERSION}\" \
+	       -DPROJECT_HOMEPAGE=\"${HOMEPAGE}\" \
+	       -DPROJECT_BUGTRACKER=\"${BUGTRACKER}\"
 CXXFLAGS     = -Wall -Wextra -pedantic -Wformat -Wformat-security \
 	       -Wconversion -Wsign-conversion
 LDFLAGS      = -static $(shell pkg-config --libs --static libarchive)
