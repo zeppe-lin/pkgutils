a1e5dc1deb3d73f5a32d460121a61793b193db56
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-13 13:44:21 +0200

    config.mk: add debug flags via DEBUG=1 make argument

--- config.mk
+++ config.mk
@@ -11,6 +11,12 @@ BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 # flags
 CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	      -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
+ifeq (${DEBUG}, 1)
+CXXFLAGS    = -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
+	      -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
+LDFLAGS     = $(shell pkg-config --libs libarchive) ${CXXFLAGS} -lasan -lubsan
+else
 CXXFLAGS    = -Wall -Wextra -pedantic -Wformat -Wformat-security \
 	      -Wconversion -Wsign-conversion
 LDFLAGS     = -static $(shell pkg-config --libs --static libarchive)
+endif
