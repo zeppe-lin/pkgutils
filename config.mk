c4f0c6996d6c5531dd78108312477fff32e430bc
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-14 23:24:33 +0200

    config.mk: to_string(), introduced recently requires C++11

--- config.mk
+++ config.mk
@@ -12,11 +12,11 @@ BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	      -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
 ifeq (${DEBUG}, 1)
-CXXFLAGS    = -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
+CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
 	      -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
 LDFLAGS     = $(shell pkg-config --libs libarchive) ${CXXFLAGS} -lasan -lubsan
 else
-CXXFLAGS    = -Wall -Wextra -pedantic -Wformat -Wformat-security \
+CXXFLAGS    = -std=c++11 -Wall -Wextra -pedantic -Wformat -Wformat-security \
 	      -Wconversion -Wsign-conversion
 LDFLAGS     = -static $(shell pkg-config --libs --static libarchive)
 endif
