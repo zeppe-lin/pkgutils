0d4b8a090af22726da5766341bfd12f6643589d6
Alexandr Savca <alexandr.savca89@gmail.com>
2024-10-02 14:53:27 +0300

    config.mk: DEBUG=y for debug, place release flags first, rm -DNDEBUG from debug flags

--- config.mk
+++ config.mk
@@ -10,14 +10,16 @@ BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
 VIMFILESDIR = ${PREFIX}/share/vim/vimfiles
 
 # flags
+ifneq (${DEBUG}, y)
 CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
 	      -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
-ifeq (${DEBUG}, 1)
-CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
-	      -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
-LDFLAGS     = $(shell pkg-config --libs libarchive) ${CXXFLAGS} -lasan -lubsan
-else
 CXXFLAGS    = -std=c++11 -Wall -Wextra -pedantic -Wformat -Wformat-security \
 	      -Wconversion -Wsign-conversion
 LDFLAGS     = -static $(shell pkg-config --libs --static libarchive)
+else
+CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
+	      -D_FILE_OFFSET_BITS=64 -DVERSION=\"${VERSION}\"
+CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
+	      -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
+LDFLAGS     = $(shell pkg-config --libs libarchive) ${CXXFLAGS} -lasan -lubsan
 endif
