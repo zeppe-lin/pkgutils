5dbe4a6aa4fdcdef6e38e19be47e19c03286b9a3
Alexandr Savca <alexandr.savca89@gmail.com>
2024-12-22 19:02:49 +0200

    config.mk: rm DIST, simplify compiler flags
    additional compiler flags can be found in extra/asan_ubsan.mk and
    extra/extra_warns.mk.

--- config.mk
+++ config.mk
@@ -1,25 +1,15 @@
 # project metadata
 NAME        = pkgutils
 VERSION     = 6.0.2
-DIST        = ${NAME}-${VERSION}
 
 # paths
 PREFIX      = /usr
-MANPREFIX   = ${PREFIX}/share/man
-BASHCOMPDIR = ${PREFIX}/share/bash-completion/completions
-VIMFILESDIR = ${PREFIX}/share/vim/vimfiles
+MANPREFIX   = $(PREFIX)/share/man
+BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
+VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
 
-# flags
-ifneq (${DEBUG}, y)
+# default compiler flags
 CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
-	      -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"${VERSION}\"
-CXXFLAGS    = -std=c++11 -Wall -Wextra -pedantic -Wformat -Wformat-security \
-	      -Wconversion -Wsign-conversion
-LDFLAGS     = -static $(shell pkg-config --libs --static libarchive)
-else
-CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
-	      -D_FILE_OFFSET_BITS=64 -DVERSION=\"${VERSION}\"
-CXXFLAGS    = -std=c++11 -ggdb3 -fno-omit-frame-pointer -fsanitize=address \
-	      -fsanitize=leak -fsanitize=undefined -fsanitize-recover=address
-LDFLAGS     = $(shell pkg-config --libs libarchive) ${CXXFLAGS} -lasan -lubsan
-endif
+              -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"$(VERSION)\"
+CXXFLAGS    = -std=c++0x -Wall -Wextra -pedantic
+LDFLAGS     = -static $(shell pkg-config --static --libs libarchive)
