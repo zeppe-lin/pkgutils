3fbe19dadda19589cf33d431b666760240278072
Alexandr Savca <alexandr.savca89@gmail.com>
2024-12-22 19:26:50 +0200

    config.mk: minor cosmetic changes

--- config.mk
+++ config.mk
@@ -8,8 +8,8 @@ MANPREFIX   = $(PREFIX)/share/man
 BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
 VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
 
-# default compiler flags
+# flags
 CPPFLAGS    = -D_POSIX_SOURCE -D_GNU_SOURCE -D_LARGEFILE_SOURCE \
               -D_FILE_OFFSET_BITS=64 -DNDEBUG -DVERSION=\"$(VERSION)\"
-CXXFLAGS    = -std=c++0x -Wall -Wextra -pedantic
+CXXFLAGS    = -std=c++0x -pedantic -Wall -Wextra
 LDFLAGS     = -static $(shell pkg-config --static --libs libarchive)
