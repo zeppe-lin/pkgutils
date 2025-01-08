455cde36c58b2306107894233728bc1a578c4802
Alexandr Savca <alexandr.savca89@gmail.com>
2025-01-08 23:27:21 +0200

    config.mk: reword a bit

--- config.mk
+++ config.mk
@@ -8,7 +8,8 @@ MANPREFIX   = $(PREFIX)/share/man
 BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
 VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
 
-# Uncomment to enable ACL extracting by pkgadd utility.
+# Uncomment to preserve packages' Access Control Lists by pkgadd.
+# See acl(5) for more information.
 # ``libarchive'' must be compiled with enabled ACL.
 # NOTE:
 #   The ACL support in libarchive is written against the POSIX1e
@@ -17,8 +18,8 @@ VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
 #   acl functions.
 #ACL        = -DENABLE_EXTRACT_ACL
 
-# Uncomment to enable "Extended Attributes" (xattrs) extracting by
-# pkgadd utility.
+# Uncomment to preserve packages' Extended attributes by pkgadd.
+# See xattr(7) for more information.
 # ``libarchive'' must be compiled with enabled xattrs support.
 #XATTR      = -DENABLE_EXTRACT_XATTR
 
