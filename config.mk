2ede525faded6818caae982939a491f671b72926
Alexandr Savca <alexandr.savca89@gmail.com>
2025-06-28 12:17:30 +0300

    config.mk: minor cosmetic changes

--- config.mk
+++ config.mk
@@ -8,19 +8,24 @@ MANPREFIX   = $(PREFIX)/share/man
 BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
 VIMFILESDIR = $(PREFIX)/share/vim/vimfiles
 
-# Uncomment to preserve packages' Access Control Lists by pkgadd.
-# See acl(5) for more information.
-# ``libarchive'' must be compiled with enabled ACL.
+# Uncomment to preserve packages' Access Control Lists by `pkgadd`.
+# See `acl(5)` for more information.
+#
+# ** `libarchive` must be compiled with enabled ACL support. **
+#
 # NOTE:
 #   The ACL support in libarchive is written against the POSIX1e
 #   draft, which was never officially approved and varies quite a bit
 #   accross platforms.  Worse, some systems have completely non-POSIX
 #   acl functions.
+#
 #ACL        = -DENABLE_EXTRACT_ACL
 
-# Uncomment to preserve packages' Extended attributes by pkgadd.
-# See xattr(7) for more information.
-# ``libarchive'' must be compiled with enabled xattrs support.
+# Uncomment to preserve packages' Extended attributes by `pkgadd`.
+# See `xattr(7)` for more information.
+#
+# ** `libarchive` must be compiled with enabled xattrs support. **
+#
 #XATTR      = -DENABLE_EXTRACT_XATTR
 
 # flags
